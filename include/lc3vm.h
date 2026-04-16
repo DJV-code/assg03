483a775b9e9f9f07d14258e90accf2fc2be29bd9
DJV-code <dvega5@leomail.tamuc.edu>
2026-04-16 00:21:05 +0000

    Tasks 1 - 6 All Pass Their Tests

--- include/lc3vm.h
+++ include/lc3vm.h
@@ -23,6 +23,14 @@
 #define BR(i) (((i) >> 6) & 0x7)
 #define FL(i) (((i) >> 11) & 1)
 #define TRP(i) ((i) & 0xFF)
+#define OPC(i) ((i) >> 12)
+#define SR2(i) ((i) & 0x0007)
+#define SR1(i) (((i) >> 6) & 0x0007)
+#define DR(i) (((i) >> 9) & 0x0007)
+#define SEXTIMM(i) (sign_extend(i, 5))
+#define OFF6(i) (sign_extend(i, 6))
+#define PCOFF9(i) (sign_extend(i, 9))
+#define PCOFF11(i) (sign_extend((i), 11))
 
 typedef void (*op_ex_f)(uint16_t i);
 typedef void (*trp_ex_f)();
@@ -78,6 +86,18 @@ void tinu16();
 void toutu16();
 void trap(uint16_t i);
 void ld_img(char* fname, uint16_t offset);
+
+uint16_t mem_read(uint16_t address);
+void mem_write(uint16_t address, uint16_t value);
+uint16_t sign_extend(uint16_t bits, int sign_position);
+void update_flags(enum registr modified_register);
+void add(uint16_t i);
+void andlc(uint16_t i);
+void notlc(uint16_t i);
+void ld(uint16_t i);
+void ldi(uint16_t i);
+void ldr(uint16_t i);
+void lea(uint16_t i);
 
 #ifdef TEST
 } // end extern C for C++ test runner
